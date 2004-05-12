a8dbad1e1e9d93e969acc0e8643429bda7e8cf97
shadowice <shadowice>
2004-05-12 22:09:41 +0000

    *** empty log message ***

--- formula/gui/ControlPanel.java
+++ formula/gui/ControlPanel.java
@@ -45,44 +45,75 @@ public class ControlPanel extends Panel {
 		this.chZoom = chZoom;
 	}
 
+	/**
+	 * Creates the control panel.
+	 */
 	public ControlPanel() {
-		btnCalcAll	= new Button(Messages.getString("ControlPanel.BtnCalcAll"));
-		btnCalcAni	= new Button(Messages.getString("ControlPanel.BtnCalcAni"));
-		btnCalcStep	= new Button(Messages.getString("ControlPanel.BtnCalcStep"));
-		btnReset	= new Button(Messages.getString("ControlPanel.BtnReset"));
-		btnVariables= new Button(Messages.getString("ControlPanel.BtnVariables"));
-		lblSpeed	= new Label(Messages.getString("ControlPanel.LblSpeed"));
-		lblFormula	= new Label();
-		sbSpeed		= new Scrollbar(Scrollbar.HORIZONTAL, 50, 10, 0, 100);
-		chZoom		= new Choice();
+		//creating all objects:
+		btnCalcAll			= new Button(Messages.getString("ControlPanel.BtnCalcAll"));
+		btnCalcAni			= new Button(Messages.getString("ControlPanel.BtnCalcAni"));
+		btnCalcStep			= new Button(Messages.getString("ControlPanel.BtnCalcStep"));
+		btnReset			= new Button(Messages.getString("ControlPanel.BtnReset"));
+		btnVariables		= new Button(Messages.getString("ControlPanel.BtnVariables"));
+		lblSpeed			= new Label(Messages.getString("ControlPanel.LblSpeed"),Label.RIGHT);
+		lblFormula			= new Label();
+		sbSpeed				= new Scrollbar(Scrollbar.HORIZONTAL, 10, 1, 0, 20);
+		chZoom				= new Choice();
+		GridBagLayout gbl	= new GridBagLayout();
+		ControlPanelListener cplistener = new ControlPanelListener();
+
 		chZoom.add("25%");
 		chZoom.add("50%");
 		chZoom.add("75%");
 		chZoom.add("100%");
 		chZoom.add("150%");
 		chZoom.add("200%");
+
+		setLayout(gbl);
 		
-		GridBagLayout gridbag=new GridBagLayout();
-		GridBagConstraints gbc=new GridBagConstraints();
-		setLayout(gridbag);
-		// lblFormula:
-		gbc.fill=GridBagConstraints.BOTH;
-		gbc.gridheight=2;
-		gridbag.setConstraints(lblFormula,gbc);
-		add(lblFormula);
-		// btnReset, btnCalcAll, btnCalcAni, btnCalcStep:
-		gbc.gridheight=1;
-		gridbag.setConstraints(btnReset,gbc);
-		add(btnReset);
-		gridbag.setConstraints(btnCalcAll,gbc);
-		add(btnCalcAll);
-		gridbag.setConstraints(btnCalcAni,gbc);
-		add(btnCalcAni);
-		gbc.gridwidth=GridBagConstraints.REMAINDER;
-		gridbag.setConstraints(btnCalcStep,gbc);
-		add(btnCalcStep);
-		gbc.weightx=0.0;
-		
+		//adding the components to this panel:
+		addComponent(this,gbl,lblFormula  ,0,0,1,2,1.0,1.0);
+		addComponent(this,gbl,btnReset	  ,1,0,1,1,0.0,0.0);
+		addComponent(this,gbl,btnCalcAni  ,2,0,1,1,0.0,0.0);
+		addComponent(this,gbl,btnCalcStep ,3,0,1,1,0.0,0.0);
+		addComponent(this,gbl,btnCalcAll  ,4,0,1,1,0.0,0.0);
+		addComponent(this,gbl,lblSpeed	  ,1,1,1,1,0.0,0.0);
+		addComponent(this,gbl,sbSpeed     ,2,1,2,1,1.0,1.0);
+		addComponent(this,gbl,btnVariables,4,1,1,1,0.0,0.0);
+	
+		//adding the listeners:
+		btnCalcAll.addActionListener(cplistener);
+		btnCalcAni.addActionListener(cplistener);
+		btnCalcStep.addActionListener(cplistener);
+		btnReset.addActionListener(cplistener);
+		btnVariables.addActionListener(cplistener);
+		sbSpeed.addAdjustmentListener(cplistener);
+		chZoom.addItemListener(cplistener);
+	}
+	
+	/**
+	 * Helper-method for adding components to the container.
+	 * Note: This should be somewhere else then in this class.
+	 * 
+	 * @param cont The container, where the component c should be added.
+	 * @param gbl The LayoutManager of "cont".
+	 * @param c The component that should be added to "cont".
+	 * @param x gridx of the GridBagConstraints
+	 * @param y gridy of the GridBagConstraints
+	 * @param width gridwidth of the GridBagConstraints
+	 * @param height gridheight of the GridBagConstraints
+	 * @param weightx weightx of the GridBagConstraints
+	 * @param weighty weighty of the GridBagConstraints
+	 */
+	private static void addComponent(Container cont,GridBagLayout gbl,Component c,int x,int y,int width,int height,double weightx,double weighty) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = x; gbc.gridy = y;
+		gbc.gridwidth = width; gbc.gridheight = height;
+		gbc.weightx = weightx; gbc.weighty = weighty;
+		gbc.insets = new Insets(2,2,2,2);
+		gbl.setConstraints( c, gbc );
+		cont.add( c );
 	}
 
 }
---
+++ formula/gui/ControlPanelListener.java
@@ -0,0 +1,56 @@
+/*
+ * Created on 12.05.2004
+ *
+ */
+package gui;
+
+import java.awt.event.*;
+import java.awt.*;
+import Messages;
+
+/**
+ * @author Maurice Gilden, Heiko Mattes, Benjamin Riehle
+ * @since 22.04.2004
+ *
+ */
+public class ControlPanelListener
+	implements ActionListener, ItemListener, AdjustmentListener {
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent event) {
+		// TODO Implement functionality to the if-blocks.
+		ControlPanel cp=(ControlPanel)((Component)event.getSource()).getParent();
+		String actionCommand=event.getActionCommand();
+		// the great and long if-statement marathon ;)
+		if(actionCommand.equals(Messages.getString("ControlPanel.BtnCalcAll"))) {
+			
+		}else if(actionCommand.equals(Messages.getString("ControlPanel.BtnCalcAni"))) {
+			
+		}else if(actionCommand.equals(Messages.getString("ControlPanel.BtnCalcStep"))) {
+			
+		}else if(actionCommand.equals(Messages.getString("ControlPanel.BtnReset"))) {
+			
+		}else if(actionCommand.equals(Messages.getString("ControlPanel.BtnVariables"))) {
+			
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ItemListener#itemStateChanged(java.awt.event.ItemEvent)
+	 */
+	public void itemStateChanged(ItemEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.AdjustmentListener#adjustmentValueChanged(java.awt.event.AdjustmentEvent)
+	 */
+	public void adjustmentValueChanged(AdjustmentEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
