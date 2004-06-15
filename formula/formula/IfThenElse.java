40e5300921c044430411966ef15b4dc558e2251f
shadowice <shadowice>
2004-06-15 16:00:24 +0000

    added constructor & small style-fixes

--- formula/formula/IfThenElse.java
+++ formula/formula/IfThenElse.java
@@ -11,9 +11,15 @@ import utils.Messages;
  *
  */
 public class IfThenElse extends MixedFormula {
-	/* (non-Javadoc)
-	 * @see formula.MixedFormula#calc()
-	 */
+
+
+	public IfThenElse() {
+		// TODO: hab nur kurz den Constructor erstellt, kA ob der so schon komplett ist.
+		formulaName = "if-then-else";
+		input = new Formula[3];
+	}
+
+
 	public final void calc() throws FormulaException {
 		if ((input[0] == null) || (input[1] == null) || (input[2] == null))
 			throw new FormulaException(Messages.getString("Error.IncompleteFormula"));
@@ -29,9 +35,7 @@ public class IfThenElse extends MixedFormula {
 				result = new Double (input[2].getDoubleResult()); }
 	}
 
-	/* (non-Javadoc)
-	 * @see formula.Formula#toString()
-	 */
+
 	public final String toString() {
 		String outString = "(If(";
 		if (input[0] != null)
@@ -48,70 +52,69 @@ public class IfThenElse extends MixedFormula {
 
 
 	public final double getDoubleResult() throws FormulaException {
-		if (result != null && result instanceof Double)
+		if ((result != null) && (result instanceof Double))
 			return ((Double)result).doubleValue();
 		else
 			throw new FormulaException(Messages.getString("Error.NoDoubleResult"));
 	}
 
-//	public final long getLongResult() {
-//		return 0;
-//	}
 
 	public final boolean getBooleanResult() throws FormulaException {
-		if (result != null && result instanceof Boolean)
+		if ((result != null) && (result instanceof Boolean))
 			return ((Boolean)result).booleanValue();
 		else
 			throw new FormulaException(Messages.getString("Error.NoBooleanResult"));
 	}
 
+
 	public final Class[] getInputTypes(int index) throws FormulaException {
-		Class[] classArray;
-		if (index == 0) {
-			classArray = new Class[1];
-			classArray[0] = Boolean.class; }
-		else if (index == 1) {
-			if (input[2] == null && output == null) {
-				classArray = new Class[2];
-				classArray[0] = Boolean.class;
-				classArray[1] = Number.class; }
-			else if (input[2] != null)
-				classArray = input[2].getOutputTypes();
-			else
-				classArray = output.getInputTypes(indexOfInput(this, output)); }
-		else if (index == 2) {
-			if (input[1] == null && output == null) {
-				classArray = new Class[2];
-				classArray[0] = Boolean.class;
-				classArray[1] = Number.class; }
-			else if (input[1] != null)
-				classArray = input[1].getOutputTypes();
-			else
-				classArray = output.getInputTypes(indexOfInput(this, output)); }
-		else {
-			throw new FormulaException(Messages.getString("Error.OutOfBound")); }
-		return classArray;
+		switch (index) {
+			case 0: 
+				return new Class[]{Boolean.class};
+			case 1:
+				if (input[2] == null && output == null) {
+					return new Class[]{Boolean.class,Number.class};
+				} else if (input[2] != null) {
+					return input[2].getOutputTypes();
+				} else {
+					return output.getInputTypes(indexOfInput(this, output));
+				}
+			case 2:
+				if (input[1] == null && output == null) {
+					return new Class[]{Boolean.class,Number.class};	
+				} else if (input[1] != null) {
+					return input[1].getOutputTypes();
+				} else {
+					return output.getInputTypes(indexOfInput(this, output));
+				}
+			default:
+				throw new FormulaException(Messages.getString("Error.OutOfBound")); 
+		}
 	}
+
 
 	public final Class[] getOutputTypes() throws FormulaException {
-		Class[] classArray;
 		if (input[1] == null && input[2] == null) {
-			classArray = new Class[2];
-			classArray[0] = Boolean.class;
-			classArray[1] = Number.class; }
-		else if (input[1] != null)
-			classArray = input[1].getOutputTypes();
-		else
-			classArray = input[2].getOutputTypes();
-		return classArray;
+			return new Class[]{Boolean.class,Number.class};
+		} else if (input[1] != null) {
+			return input[1].getOutputTypes();
+		} else {
+			return input[2].getOutputTypes();
+		}
 	}
 
-//	public final boolean isValidInput(Formula in, int whichInput) {
-//		return false;
-//	}
-//
-//	public final boolean isValidOutput(Formula in, int whichInput) {
-//		return false;
-//	}
+	// TODO: Zum kurz mal testen, löschen wenn's nicht mehr gebraucht wird!	
+	public static void main(String[] args){
+		IfThenElse ite=new IfThenElse();
+		ite.setInput(ite,0);
+		try{
+			Class[] itypes=ite.getInputTypes(1);
+			for(int i=0;i<itypes.length;i++){
+				System.out.println(itypes[i].getName());
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
 }
