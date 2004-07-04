2f8bc9055c1b78d9f7218db9f67423c50abdd411
br3001 <br3001>
2004-07-04 23:46:00 +0000

    Jetzt neu!!! (unspektakulär)

---
+++ formula/formula/Arctan.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 05.07.2004
+ *
+ */
+package formula;
+
+import utils.Messages;
+
+/**
+ * @author Maurice Gilden, Heiko Mattes, Benjamin Riehle
+ *
+ */
+public class Arctan extends NumberFormula {
+
+	public final void calc() throws FormulaException {
+		if (input[0] == null)
+			throw new FormulaException(Messages.getString("Error.IncompleteFormula"));
+		else
+			result = new Double (Math.atan(input[0].getDoubleResult()));
+	}
+
+	/**
+	 * Creates a "arctan" with 1 input.
+	 */
+	public Arctan() {
+		input = new Formula[1];
+		formulaName = "arctan";
+	}
+	
+	/**
+	 * @return Returns the string-equivalent of this formula-object (if inputs not connected)
+	 * or the resulting formula as string for a (sub)tree with this object as root.
+	 */	
+	public final String toString() {
+		String outString = "(arctan ";
+		if (input[0] != null)
+			outString += input[0].toString();
+		outString += ")";
+		return outString;
+	}
+
+}
---
+++ formula/formula/Ln.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 05.07.2004
+ *
+ */
+package formula;
+
+import utils.Messages;
+
+/**
+ * @author Maurice Gilden, Heiko Mattes, Benjamin Riehle
+ *
+ */
+public class Ln extends NumberFormula {
+
+	public final void calc() throws FormulaException {
+		if (input[0] == null)
+			throw new FormulaException(Messages.getString("Error.IncompleteFormula"));
+		else
+			result = new Double (Math.log(input[0].getDoubleResult()));
+	}
+
+	/**
+	 * Creates a "Ln" with 1 input.
+	 */
+	public Ln() {
+		input = new Formula[1];
+		formulaName = "ln";
+	}
+	
+	/**
+	 * @return Returns the string-equivalent of this formula-object (if inputs not connected)
+	 * or the resulting formula as string for a (sub)tree with this object as root.
+	 */	
+	public final String toString() {
+		String outString = "(ln ";
+		if (input[0] != null)
+			outString += input[0].toString();
+		outString += ")";
+		return outString;
+	}
+
+}
