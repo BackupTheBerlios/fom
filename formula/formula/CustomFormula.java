0b0d6fdf458b9d2a31cf12087e9ee2b396d63fdd
ot_piccolo <ot_piccolo>
2004-09-02 19:59:05 +0000

    Freude! CustomFormula ist fertig (hoffe ich).

--- formula/formula/CustomFormula.java
+++ formula/formula/CustomFormula.java
@@ -13,23 +13,38 @@ public class CustomFormula extends Formula {
 	protected Object result;
 
 	protected Formula root;
-	protected String[] mappingInputsVariables;
+	protected VariableList variables;
 	protected Class[] validInputTypes;
 
-	public CustomFormula(Formula root) throws FormulaException {
-		if ((root != null) && root.isCompleteSubTree()) {
-
-		} else {
+	public CustomFormula(String name, Formula root, VariableList variables) throws FormulaException {
+		super();
+		if ((root == null) || ! root.isCompleteSubTree()) {
 			throw new FormulaException(Messages.getString("Error.IncompleteFormula"));
+		} else {
+			this.variables = variables;
+			this.root = root;
+			input = new Formula[variables.size()];
+			formulaName = name;
 		}
 	}
 
-	private final void createTree(Formula formulaNode, Formula customNode, Formula customNodeFather) {
+	private final void mapInputsOnVariables() throws FormulaException {
+		TypeConstVar variable;
+		for (int i=0; i < getInputCount(); i++) {
+			if (getInput(i) == null) {
+				throw new FormulaException(Messages.getString("Error.IncompleteFormula"));
+			} else {
+				variable = (TypeConstVar)variables.get(i);
+				if (getInput(i).hasBooleanResult()) {
+					variable.setValue(new Boolean(getInput(i).getBooleanResult()));
+				} else if (getInput(i).hasDoubleResult()) {
+					variable.setValue(new Double(getInput(i).getDoubleResult()));
+				} else {
+					throw new FormulaException(Messages.getString("Error.IllegalDataType"));
+				}
+			}
+		}
 		
-	}
-
-	private final void setMapping() {
-		 
 	}
 
 	/**
@@ -37,6 +52,16 @@ public class CustomFormula extends Formula {
 	 */
 	public final void clearResult() {
 		result = null;
+		clearResultTree(root);
+	}
+
+	private final void clearResultTree(Formula root) {
+		root.clearResult();
+		for (int i=0; i < root.getInputCount(); i++) {
+			if (root.getInput(i) != null) {
+				clearResultTree(root.getInput(i));
+			}
+		}
 	}
 
 	public final double getDoubleResult() throws FormulaException {
@@ -80,19 +105,35 @@ public class CustomFormula extends Formula {
 	}
 
 	public final void calc() throws FormulaException {
-		
+		mapInputsOnVariables();
+		root.calc();
+		if (root.hasBooleanResult()) {
+			result = new Boolean(root.getBooleanResult());
+		} else if (root.hasDoubleResult()) {
+			result = new Double(root.getDoubleResult());
+		} else {
+			throw new FormulaException(Messages.getString("Error.IllegalDataType"));
+		}
 	}
 
 	public final String toString() {
-		return "";
+		String outString = formulaName + "(";
+		for (int i=0; i < variables.size(); i++) {
+			outString += getInput(i).toString();
+			if (i+1 != variables.size()) {
+				outString += ",";
+			}
+		}
+		return outString + ")";
 	}
 
 	public final Class[] getInputTypes(int index) throws FormulaException {
-		return new Class[] {Object.class};
+		TypeConstVar variable = (TypeConstVar)variables.get(index);
+		return new Class[] {variable.getValue().getClass()};
 	}
 
 	public final Class[] getOutputTypes() throws FormulaException {
-		return new Class[] {Object.class};
+		return root.getOutputTypes();
 	}
 
 }
