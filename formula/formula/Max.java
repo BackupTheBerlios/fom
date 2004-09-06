a474b7887a8e291899537099e9f4d9664dfec3d3
br3001 <br3001>
2004-09-06 15:15:48 +0000

    *** empty log message ***

--- formula/formula/Max.java
+++ formula/formula/Max.java
@@ -34,7 +34,7 @@ public class Max extends NumberFormula {
 	 * or the resulting formula as string for a (sub)tree with this object as root.
 	 */	
 	public final String toString() {
-		String outString = "max (";
+		String outString = "max(";
 		if (input[0] != null)
 			outString += input[0].toString();
 		outString += "; ";
